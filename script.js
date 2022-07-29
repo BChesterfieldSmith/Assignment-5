3832c81953a7b509b2da4c33f956f974f6183c36
BChesterfieldSMith <bryan_smith2@cable.comcast.com>
2022-07-29 16:06:41 -0400

    Part1 Done

--- script.js
+++ script.js
@@ -11,3 +11,34 @@
 </ul>
 <img src="${}">
 */
+
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event){      
+       
+      let pilotName = document.querySelector("input[name=pilotName");
+      let copilotName = document.querySelector("input[name=copilotName"); 
+      let fuelLevel = document.querySelector("input[name=fuelLevel");
+      let cargoMass = document.querySelector("input[name=cargoMass");
+      
+       let pilotNumCheck = Number(pilotName.value);
+       let coPilotNumCheck = Number(copilotName.value);
+       let fuelLevelCheck = Number(fuelLevel.value);
+       let cargoMassCheck = Number(cargoMass.value);
+       
+       
+       if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+               alert("All fields must be filled out"); 
+           } else if (isNaN(pilotNumCheck) === false || isNaN(coPilotNumCheck) === false || isNaN(fuelLevelCheck) === true || isNaN(cargoMassCheck)===true) {
+               alert("All fields must be correct data type")
+   
+           } else { 
+             
+            alert("Inputs are correct confirmation")
+
+           }
+            event.preventDefault();
+              
+       });   
+   });
+  
